b0d0afd90ff484bfff8a3739fc05a6826f7c5765
PRIYADHARSHINI A <janupriya1612@gmail.com>
2025-08-02 16:00:10 +0530

    Movie Webapp

---
+++ Movie webapp/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+
+# Load the model
+model1 = pickle.load(open('C:/Users/priya/PycharmProjects/MOVIE_WEBAPP/data/model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')  # The form to input values
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get input from HTML form
+    val1 = float(request.form['feature1'])
+    val2 = float(request.form['feature2'])
+    val3 = float(request.form['feature3'])
+    val4 = float(request.form['feature4'])
+
+    # Format input data for prediction
+    input_data = [[val1, val2], [val3, val4]]  # Two users' inputs
+    predictions = model1.predict(input_data)
+
+    return render_template('result.html', prediction=predictions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
